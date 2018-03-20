60c13b87689cad0d7b6288747879da2bb57ab010
odair <odair.santiago@prodaptech.com.br>
2018-03-20 15:55:39 -0300

    Melhorando nosso código de teste com Page Objects

--- testes-de-sistema/src/br/com/caelum/teste/UsuarioSystemTest.java
+++ testes-de-sistema/src/br/com/caelum/teste/UsuarioSystemTest.java
@@ -1,112 +1,40 @@
 package br.com.caelum.teste;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UsuarioSystemTest {
+
 	private WebDriver driver;
+    private UsuariosPage usuarios;
 
-	@Before
-	public void inicializa() {
-	    	 
-	    	System.setProperty("webdriver.gecko.driver", "D:\\Alura\\geckodriver-v0.20.0-win64\\geckodriver.exe");
-	        driver = new FirefoxDriver();
-	    }
-
-	    @Test	
-	public void validarAddUsuario() {
-		driver.get("http://localhost:8080/usuarios/new");
-		
-		WebElement nome = driver.findElement(By.name("usuario.nome"));
-		WebElement email = driver.findElement(By.name("usuario.email"));
-		
-		nome.sendKeys("Usuario 1 Nome");
-		email.sendKeys("usuario1@com.com.br");
-		
-		// submete o form
-		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
-		botaoSalvar.click();
-		
-		//Garantir que o user foi inserido
-		boolean achouNome =  driver.getPageSource().contains("Usuario 1 Nome");
-		boolean achouemail =  driver.getPageSource().contains("usuario1@com.com.br");
-		
-		assertTrue(achouNome);	
-		assertTrue(achouemail);	
-
-			
-	}
-
-	@Test
-	public void validarAddUsuarioNomeObrigatorio() {
-		driver.get("http://localhost:8080/usuarios/new");
-		
-		WebElement email = driver.findElement(By.name("usuario.email"));
-		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
-		
-		email.sendKeys("usuario2@com.com.br");
-		botaoSalvar.click();
-		
-		//Garantir a mensagem apareceu
-		assertTrue(driver.getPageSource().contains("Nome obrigatorio!"));
-
-	}
-	
-	@Test
-	public void validarAddUsuarioEmailObrigatorio() {
-		driver.get("http://localhost:8080/usuarios/new");
-		
-		WebElement nome = driver.findElement(By.name("usuario.nome"));
-		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
-		
-		nome.sendKeys("Usuario 3 Nome");
-		botaoSalvar.click();
-
-		//Garantir a mensagem apareceu
-		assertTrue(driver.getPageSource().contains("E-mail obrigatorio!"));
-
-	}
-
-	@Test
-    public void validarAddUsuarioNomeEmailObrigatorio() {
-		driver.get("http://localhost:8080/usuarios/new");
-		
-		WebElement nome = driver.findElement(By.name("usuario.nome"));
-        WebElement email = driver.findElement(By.name("usuario.email"));
-        email.submit();
-
-        assertTrue(driver.getPageSource().contains("Nome obrigatorio!"));
-        assertTrue(driver.getPageSource().contains("E-mail obrigatorio!"));
-
-  
+    @Before
+    public void inicializa() {
+    	//Abrindo pelo firefox
+    	//System.setProperty("webdriver.gecko.driver", "D:\\Alura\\geckodriver-v0.20.0-win64\\geckodriver.exe");
+        //this.driver = new FirefoxDriver();
+        //Abrindo pelo Chrome
+        System.setProperty("webdriver.chrome.driver","D:\\Alura\\chromedriver\\chromedriver.exe");
+        this.driver = new ChromeDriver();
+        this.usuarios = new UsuariosPage(driver);
     }
-	
-	@Test
-	public void navegarLink() {
-	        driver.get("http://localhost:8080/usuarios");
-	        WebElement ligacao = driver.findElement(By.linkText("Novo Usuário"));
-	        ligacao.click();        
 
-	        //garantir que abriu pagina de inclusao
-	        boolean achouCampoNome = driver.getPageSource().contains("Nome:");
-	        boolean achouCampoEmail = driver.getPageSource().contains("E-mail:");
+    @Test
+    public void deveAdicionarUmUsuario() {
+        usuarios.visita();
+        usuarios.novo().cadastra("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br");
 
-	        assertTrue(achouCampoNome);
-	        assertTrue(achouCampoEmail);
+        assertTrue(usuarios.existeNaListagem("Ronaldo Luiz de Albuquerque", "ronaldo2009@terra.com.br"));
 
-	}
-	
-	
-	
-	@After
-	public void encerra() {
-	        driver.close();
-	    }
+    }
+
+    @After
+    public void encerra() {
+        driver.close();
+    }
 }
