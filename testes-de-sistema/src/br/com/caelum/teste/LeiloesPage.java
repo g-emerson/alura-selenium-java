8558bfc7bcc28fe3d67dfb3d7dba2e73fb40d6e5
odair <odair.santiago@prodaptech.com.br>
2018-03-21 11:43:30 -0300

    Esperando por Requisições Ajax

--- testes-de-sistema/src/br/com/caelum/teste/LeiloesPage.java
+++ testes-de-sistema/src/br/com/caelum/teste/LeiloesPage.java
@@ -1,7 +1,10 @@
 package br.com.caelum.teste;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LeiloesPage {
 	private WebDriver driver;
@@ -27,6 +30,12 @@ public class LeiloesPage {
         return driver.getPageSource().contains(produto) && 
                 driver.getPageSource().contains(String.valueOf(valor)) &&
                 driver.getPageSource().contains(usado ? "Sim" : "Não");
+    }
+    
+    public DetalhesDoLeilaoPage detalhes(int posicao) {
+        List<WebElement> elementos = driver.findElements(By.linkText("exibir"));
+        elementos.get(posicao - 1).click();
 
+        return new DetalhesDoLeilaoPage(driver);
     }
 }
